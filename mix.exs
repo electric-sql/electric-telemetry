818dc5165a868abc80545da5531d70bc62cecc7c
Oleksii Sholik <oleksii@sholik.dev>
2025-10-21 18:06:16 +0200

    Import telemetry modules from Electric

--- mix.exs
+++ mix.exs
@@ -11,18 +11,21 @@ defmodule Electric.Telemetry.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :os_mon, :runtime_tools]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:otel_metric_exporter, "~> 0.3.11"},
+      {:postgrex, "~> 0.21"},
+      {:req, "~> 0.5"},
+      {:telemetry, "~> 1.3"},
+      {:telemetry_metrics, "~> 1.1"},
+      {:telemetry_metrics_prometheus_core, "~> 1.2"},
+      {:telemetry_poller, "~> 1.3"}
     ]
   end
 end
