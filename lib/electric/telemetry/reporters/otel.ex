339a07d48a0b5e58ddd79c4c00b790901af7c676
Oleksii Sholik <oleksii@sholik.dev>
2025-11-07 12:52:08 +0100

    Otel

--- lib/electric/telemetry/reporters/otel.ex
+++ lib/electric/telemetry/reporters/otel.ex
@@ -1,24 +1,29 @@
 defmodule Electric.Telemetry.Reporters.Otel do
   def child_spec(telemetry_opts, reporter_opts) do
     if get_in(telemetry_opts, [:reporters, :otel_metrics?]) do
-      {__MODULE__, Enum.into(reporter_opts, telemetry_opts)}
+      {__MODULE__, [telemetry_opts: telemetry_opts] ++ reporter_opts}
     end
   end
 
-  def start_link(telemetry_opts) do
-    OtelMetricExporter.start_link(
-      metrics: Keyword.fetch!(telemetry_opts, :metrics),
-      export_period: get_in(telemetry_opts, [:reporters, :otel_export_period]),
+  def start_link(opts) do
+    {telemetry_opts, opts} = Keyword.pop(opts, :telemetry_opts)
+    reporters = telemetry_opts.reporters
+
+    installation_id = telemetry_opts[:installation_id] || "electric_default"
+
+    resource =
+      Map.merge(
+        %{instance: %{installation_id: installation_id}},
+        reporters.otel_resource_attributes
+      )
+
+    start_opts = [
+      metrics: Keyword.fetch!(opts, :metrics),
+      export_period: reporters.otel_export_period,
       # TODO: add stack_id for stack telemetry
-      resource:
-        Map.merge(
-          %{
-            instance: %{
-              installation_id: Map.get(telemetry_opts, :installation_id, "electric_default")
-            }
-          },
-          get_in(telemetry_opts, [:reporters, :otel_resource_attributes])
-        )
-    )
+      resource: resource
+    ]
+
+    OtelMetricExporter.start_link(start_opts)
   end
 end
