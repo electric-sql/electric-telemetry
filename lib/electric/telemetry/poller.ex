fe25c4444a987c9317d3959707f30fa6b38dba70
Oleksii Sholik <oleksii@sholik.dev>
2025-11-10 23:39:02 +0100

    Gate poller and system_monitor behind compilation flags

--- lib/electric/telemetry/poller.ex
+++ lib/electric/telemetry/poller.ex
@@ -1,43 +1,47 @@
-defmodule Electric.Telemetry.Poller do
-  @callback builtin_periodic_measurements(map) :: list()
+use Electric.Telemetry
 
-  def child_spec(telemetry_opts, poller_opts) do
-    {callback_module, opts} = Keyword.pop(poller_opts, :callback_module)
+with_telemetry [Telemetry.Metrics, :telemetry_poller] do
+  defmodule Electric.Telemetry.Poller do
+    @callback builtin_periodic_measurements(map) :: list()
 
-    case periodic_measurements(telemetry_opts, callback_module) do
-      [] ->
-        nil
+    def child_spec(telemetry_opts, poller_opts) do
+      {callback_module, opts} = Keyword.pop(poller_opts, :callback_module)
 
-      measurements ->
-        opts =
-          Keyword.merge(
-            [
-              measurements: measurements,
-              period: telemetry_opts.intervals_and_thresholds.system_metrics_poll_interval,
-              init_delay: :timer.seconds(5)
-            ],
-            opts
-          )
+      case periodic_measurements(telemetry_opts, callback_module) do
+        [] ->
+          nil
 
-        {:telemetry_poller, opts}
+        measurements ->
+          opts =
+            Keyword.merge(
+              [
+                measurements: measurements,
+                period: telemetry_opts.intervals_and_thresholds.system_metrics_poll_interval,
+                init_delay: :timer.seconds(5)
+              ],
+              opts
+            )
+
+          {:telemetry_poller, opts}
+      end
     end
+
+    def periodic_measurements(%{periodic_measurements: measurements} = telemetry_opts, module) do
+      Enum.flat_map(measurements, fn
+        :builtin -> module.builtin_periodic_measurements(telemetry_opts)
+        # These are implemented by telemetry_poller
+        f when f in [:memory, :persistent_term, :system_counts, :total_run_queue_lengths] -> [f]
+        # Bare function names are assumed to be referring to functions defined in the caller module
+        f when is_atom(f) -> {module, f, [telemetry_opts]}
+        f when is_function(f, 1) -> {__MODULE__, :user_measurement, [f, telemetry_opts]}
+        {m, f, a} when is_atom(m) and is_atom(f) and is_list(a) -> [{m, f, a ++ [telemetry_opts]}]
+      end)
+    end
+
+    def periodic_measurements(telemetry_opts, module),
+      do: module.builtin_periodic_measurements(telemetry_opts)
+
+    # Helper function to enable telemetry_poller to call a user-provided anonymous function
+    def user_measurement(f, telemetry_opts), do: f.(telemetry_opts)
   end
-
-  def periodic_measurements(%{periodic_measurements: measurements} = telemetry_opts, module) do
-    Enum.flat_map(measurements, fn
-      :builtin -> module.builtin_periodic_measurements(telemetry_opts)
-      # These are implemented by telemetry_poller
-      f when f in [:memory, :persistent_term, :system_counts, :total_run_queue_lengths] -> [f]
-      # Bare function names are assumed to be referring to functions defined in the caller module
-      f when is_atom(f) -> {module, f, [telemetry_opts]}
-      f when is_function(f, 1) -> {__MODULE__, :user_measurement, [f, telemetry_opts]}
-      {m, f, a} when is_atom(m) and is_atom(f) and is_list(a) -> [{m, f, a ++ [telemetry_opts]}]
-    end)
-  end
-
-  def periodic_measurements(telemetry_opts, module),
-    do: module.builtin_periodic_measurements(telemetry_opts)
-
-  # Helper function to enable telemetry_poller to call a user-provided anonymous function
-  def user_measurement(f, telemetry_opts), do: f.(telemetry_opts)
 end
--- lib/electric/telemetry/system_monitor.ex
+++ lib/electric/telemetry/system_monitor.ex
@@ -1,143 +1,149 @@
-defmodule Electric.Telemetry.SystemMonitor do
-  @moduledoc """
-  Application-wide process that initializes Erlang's system monitor and consumes monitoring events.
+use Electric.Telemetry
 
-  Currently the follow events are tracked:
+with_telemetry Telemetry.Metrics do
+  defmodule Electric.Telemetry.SystemMonitor do
+    @moduledoc """
+    Application-wide process that initializes Erlang's system monitor and consumes monitoring events.
 
-    - long_gc
-    - long_schedule
-    - long_message_queue
+    Currently the follow events are tracked:
 
-  """
+      - long_gc
+      - long_schedule
+      - long_message_queue
 
-  use GenServer
+    """
 
-  import Electric.Telemetry.Processes, only: [proc_type: 1]
+    use GenServer
 
-  require Logger
+    import Electric.Telemetry.Processes, only: [proc_type: 1]
 
-  @vm_monitor_long_gc [:vm, :monitor, :long_gc]
-  @vm_monitor_long_schedule [:vm, :monitor, :long_schedule]
-  @vm_monitor_long_message_queue [:vm, :monitor, :long_message_queue]
+    require Logger
 
-  def start_link(opts) do
-    GenServer.start_link(__MODULE__, opts.intervals_and_thresholds, name: __MODULE__)
-  end
+    @vm_monitor_long_gc [:vm, :monitor, :long_gc]
+    @vm_monitor_long_schedule [:vm, :monitor, :long_schedule]
+    @vm_monitor_long_message_queue [:vm, :monitor, :long_message_queue]
 
-  def init(opts) do
-    :erlang.system_monitor(self(),
-      long_gc: opts.long_gc_threshold,
-      long_schedule: opts.long_schedule_threshold,
-      long_message_queue:
-        {opts.long_message_queue_disable_threshold, opts.long_message_queue_enable_threshold}
-    )
+    def start_link(opts) do
+      GenServer.start_link(__MODULE__, opts.intervals_and_thresholds, name: __MODULE__)
+    end
 
-    {:ok, %{long_message_queue_pids: %{}, long_message_queue_timer: nil}}
-  end
+    def init(opts) do
+      :erlang.system_monitor(self(),
+        long_gc: opts.long_gc_threshold,
+        long_schedule: opts.long_schedule_threshold,
+        long_message_queue:
+          {opts.long_message_queue_disable_threshold, opts.long_message_queue_enable_threshold}
+      )
 
-  def handle_info({:monitor, gc_pid, :long_gc, info}, state) do
-    type = proc_type(gc_pid)
+      {:ok, %{long_message_queue_pids: %{}, long_message_queue_timer: nil}}
+    end
 
-    Logger.debug(
-      "Long GC detected for pid #{inspect(gc_pid)} (#{inspect(type)}): took #{Keyword.fetch!(info, :timeout)}ms. #{inspect(info, limit: :infinity)}"
-    )
+    def handle_info({:monitor, gc_pid, :long_gc, info}, state) do
+      type = proc_type(gc_pid)
 
-    :telemetry.execute(@vm_monitor_long_gc, Map.new(info), %{process_type: type})
+      Logger.debug(
+        "Long GC detected for pid #{inspect(gc_pid)} (#{inspect(type)}): took #{Keyword.fetch!(info, :timeout)}ms. #{inspect(info, limit: :infinity)}"
+      )
 
-    {:noreply, state}
-  end
+      :telemetry.execute(@vm_monitor_long_gc, Map.new(info), %{process_type: type})
 
-  def handle_info({:monitor, port, :long_schedule, info}, state) when is_port(port) do
-    Logger.debug(
-      "Long schedule detected for port #{inspect(port)}, took #{Keyword.fetch!(info, :timeout)}ms"
-    )
+      {:noreply, state}
+    end
 
-    :telemetry.execute(@vm_monitor_long_schedule, Map.new(info), %{process_type: :port})
+    def handle_info({:monitor, port, :long_schedule, info}, state) when is_port(port) do
+      Logger.debug(
+        "Long schedule detected for port #{inspect(port)}, took #{Keyword.fetch!(info, :timeout)}ms"
+      )
 
-    {:noreply, state}
-  end
+      :telemetry.execute(@vm_monitor_long_schedule, Map.new(info), %{process_type: :port})
 
-  def handle_info({:monitor, pid, :long_schedule, info}, state) when is_pid(pid) do
-    type = proc_type(pid)
+      {:noreply, state}
+    end
 
-    Logger.debug(fn ->
-      locations =
-        info
-        |> Keyword.delete(:timeout)
-        |> Map.new(fn {loc, {m, f, a}} when loc in [:in, :out] ->
-          {loc, Exception.format_mfa(m, f, a)}
-        end)
+    def handle_info({:monitor, pid, :long_schedule, info}, state) when is_pid(pid) do
+      type = proc_type(pid)
 
-      locs_str =
-        if map_size(locations) > 0 do
-          "; " <>
-            (locations |> Enum.map(fn {key, val} -> "#{key}: #{val}" end) |> Enum.join(", "))
-        else
-          ""
-        end
+      Logger.debug(fn ->
+        locations =
+          info
+          |> Keyword.delete(:timeout)
+          |> Map.new(fn {loc, {m, f, a}} when loc in [:in, :out] ->
+            {loc, Exception.format_mfa(m, f, a)}
+          end)
 
-      "Long schedule detected for pid #{inspect(pid)} (#{inspect(type)}), took #{Keyword.fetch!(info, :timeout)}ms" <>
-        locs_str
-    end)
+        locs_str =
+          if map_size(locations) > 0 do
+            "; " <>
+              (locations |> Enum.map(fn {key, val} -> "#{key}: #{val}" end) |> Enum.join(", "))
+          else
+            ""
+          end
 
-    :telemetry.execute(@vm_monitor_long_schedule, %{timeout: Keyword.fetch!(info, :timeout)}, %{
-      process_type: type
-    })
+        "Long schedule detected for pid #{inspect(pid)} (#{inspect(type)}), took #{Keyword.fetch!(info, :timeout)}ms" <>
+          locs_str
+      end)
 
-    {:noreply, state}
-  end
-
-  def handle_info({:monitor, pid, :long_message_queue, true}, state) do
-    type = proc_type(pid)
-
-    Logger.debug("Long message queue detected for pid #{inspect(pid)} (#{inspect(type)})")
-
-    log_long_message_queue_event(pid, type)
-
-    state =
-      %{state | long_message_queue_pids: Map.put(state.long_message_queue_pids, pid, type)}
-      |> maybe_start_long_message_queue_timer()
-
-    {:noreply, state}
-  end
-
-  def handle_info({:monitor, pid, :long_message_queue, false}, state) do
-    Logger.debug("Long message queue no longer detected for pid #{inspect(pid)}")
-    {:noreply, %{state | long_message_queue_pids: Map.delete(state.long_message_queue_pids, pid)}}
-  end
-
-  def handle_info(:recheck_message_queues, state)
-      when map_size(state.long_message_queue_pids) == 0 do
-    :timer.cancel(state.long_message_queue_timer)
-    {:noreply, %{state | long_message_queue_timer: nil}}
-  end
-
-  def handle_info(:recheck_message_queues, state) do
-    Enum.each(state.long_message_queue_pids, fn {pid, type} ->
-      log_long_message_queue_event(pid, type)
-    end)
-
-    {:noreply, state}
-  end
-
-  defp log_long_message_queue_event(pid, type) do
-    with {:message_queue_len, queue_len} <- Process.info(pid, :message_queue_len) do
-      :telemetry.execute(@vm_monitor_long_message_queue, %{length: queue_len}, %{
+      :telemetry.execute(@vm_monitor_long_schedule, %{timeout: Keyword.fetch!(info, :timeout)}, %{
         process_type: type
       })
+
+      {:noreply, state}
     end
+
+    def handle_info({:monitor, pid, :long_message_queue, true}, state) do
+      type = proc_type(pid)
+
+      Logger.debug("Long message queue detected for pid #{inspect(pid)} (#{inspect(type)})")
+
+      log_long_message_queue_event(pid, type)
+
+      state =
+        %{state | long_message_queue_pids: Map.put(state.long_message_queue_pids, pid, type)}
+        |> maybe_start_long_message_queue_timer()
+
+      {:noreply, state}
+    end
+
+    def handle_info({:monitor, pid, :long_message_queue, false}, state) do
+      Logger.debug("Long message queue no longer detected for pid #{inspect(pid)}")
+
+      {:noreply,
+       %{state | long_message_queue_pids: Map.delete(state.long_message_queue_pids, pid)}}
+    end
+
+    def handle_info(:recheck_message_queues, state)
+        when map_size(state.long_message_queue_pids) == 0 do
+      :timer.cancel(state.long_message_queue_timer)
+      {:noreply, %{state | long_message_queue_timer: nil}}
+    end
+
+    def handle_info(:recheck_message_queues, state) do
+      Enum.each(state.long_message_queue_pids, fn {pid, type} ->
+        log_long_message_queue_event(pid, type)
+      end)
+
+      {:noreply, state}
+    end
+
+    defp log_long_message_queue_event(pid, type) do
+      with {:message_queue_len, queue_len} <- Process.info(pid, :message_queue_len) do
+        :telemetry.execute(@vm_monitor_long_message_queue, %{length: queue_len}, %{
+          process_type: type
+        })
+      end
+    end
+
+    defp maybe_start_long_message_queue_timer(%{long_message_queue_timer: nil} = state) do
+      # A process whose message queue length exceeds the threshold is likely to be spiraling out of
+      # control. Therefore we need recheck it quite often to capture the dynamics.
+      #
+      # Though there's still no guarantee that the VM will not run out of memory before it
+      # reaches the next metric export tick.
+      {:ok, timer} = :timer.send_interval(200, :recheck_message_queues)
+
+      %{state | long_message_queue_timer: timer}
+    end
+
+    defp maybe_start_long_message_queue_timer(state), do: state
   end
-
-  defp maybe_start_long_message_queue_timer(%{long_message_queue_timer: nil} = state) do
-    # A process whose message queue length exceeds the threshold is likely to be spiraling out of
-    # control. Therefore we need recheck it quite often to capture the dynamics.
-    #
-    # Though there's still no guarantee that the VM will not run out of memory before it
-    # reaches the next metric export tick.
-    {:ok, timer} = :timer.send_interval(200, :recheck_message_queues)
-
-    %{state | long_message_queue_timer: timer}
-  end
-
-  defp maybe_start_long_message_queue_timer(state), do: state
 end
