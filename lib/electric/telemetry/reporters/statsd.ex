3df60a47c3c8314d5b4bfaad3e0537e88d6cee47
Oleksii Sholik <oleksii@sholik.dev>
2025-11-11 13:42:24 +0100

    Fix Statsd reporter's setup

--- lib/electric/telemetry/reporters/statsd.ex
+++ lib/electric/telemetry/reporters/statsd.ex
@@ -4,22 +4,18 @@ with_telemetry Telemetry.Metrics do
   defmodule Electric.Telemetry.Reporters.Statsd do
     def child_spec(telemetry_opts, reporter_opts) do
       if host = get_in(telemetry_opts, [:reporters, :statsd_host]) do
-        {__MODULE__,
-         [host: host, global_tags: [instance_id: telemetry_opts.instance_id] ++ reporter_opts]}
+        start_opts =
+          Keyword.merge(
+            [
+              host: host,
+              formatter: :datadog,
+              global_tags: [instance_id: telemetry_opts.instance_id]
+            ],
+            reporter_opts
+          )
+
+        {TelemetryMetricsStatsd, start_opts}
       end
-    end
-
-    def start_link(opts) do
-      host = Keyword.fetch!(opts, :host)
-      global_tags = Keyword.fetch!(opts, :global_tags)
-      metrics = Keyword.fetch!(opts, :metrics)
-
-      TelemetryMetricsStatsd.start_link(
-        host: host,
-        formatter: :datadog,
-        global_tags: global_tags,
-        metrics: metrics
-      )
     end
 
     def add_instance_id_tag(metrics) do
