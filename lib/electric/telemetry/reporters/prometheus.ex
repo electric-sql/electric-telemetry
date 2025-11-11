a8ef570335b4d211589d6c3881dd6769b59cc24a
Oleksii Sholik <oleksii@sholik.dev>
2025-11-11 13:43:28 +0100

    Fix Prometheus reporter's setup

--- lib/electric/telemetry/reporters/prometheus.ex
+++ lib/electric/telemetry/reporters/prometheus.ex
@@ -4,12 +4,8 @@ with_telemetry Telemetry.Metrics do
   defmodule Electric.Telemetry.Reporters.Prometheus do
     def child_spec(telemetry_opts, reporter_opts) do
       if get_in(telemetry_opts, [:reporters, :prometheus?]) do
-        reporter_opts
+        {TelemetryMetricsPrometheus.Core, reporter_opts}
       end
-    end
-
-    def start_link(opts) do
-      TelemetryMetricsPrometheus.Core.start_link(opts)
     end
   end
 end
